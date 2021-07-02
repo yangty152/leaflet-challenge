86ea864839a45e0012e7c696804a4af5c47997a3
yangty152 <35477773+yangty152@users.noreply.github.com>
2021-07-01 21:23:49 -0400

    read in data set

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -16,47 +16,31 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-// Create a new marker
-L.marker([45.52, -122.67]).addTo(myMap);
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson").then( data => {
+    console.log(data.features);
+})
 
-// Create a circle and pass in some initial options
-L.circle([45.52, -122.69], {
-  color: "green",
-  fillColor: "green",
-  fillOpacity: 0.75,
-  radius: 500
-}).addTo(myMap);
-
-// Create a Polygon and pass in some initial options
-L.polygon([
-  [45.54, -122.68],
-  [45.55, -122.68],
-  [45.55, -122.66]
-], {
-  color: "yellow",
-  fillColor: "yellow",
-  fillOpacity: 0.75
-}).addTo(myMap);
-
-// Coordinates for each point to be used in the polyline
-var line = [
-  [45.51, -122.68],
-  [45.50, -122.60],
-  [45.48, -122.70],
-  [45.54, -122.75]
-];
-
-// Create a polyline using the line coordinates and pass in some initial options
-L.polyline(line, {
-  color: "red"
-}).addTo(myMap);
-
-// Create a rectangle and pass in some initial options
-L.rectangle([
-  [45.55, -122.64],
-  [45.54, -122.61]
-], {
-  color: "black",
-  weight: 3,
-  stroke: true
-}).addTo(myMap);
+function fillcolor(points){
+    if (points >=200) {
+      return 'blue';
+    } else if (points >=150 && points <199) {
+      return 'green';
+    } else if (points >=99 && points <150) {
+      return 'yellow';
+    } else {
+      return 'red';
+    };
+  }
+  function markerSize(population){
+    return population/10; 
+  }
+  for (var i = 0; i < countries.length; i++){
+    var country = countries[i];
+    L.circleMarker(country.location, {
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: fillcolor(country.points),
+      radius: markerSize(country.points)
+    }).bindPopup("<h1>" + country.name + "</h1> <hr> <h3>Population: " + country.points + "</h3>").addTo(myMap);
+  }
+  
