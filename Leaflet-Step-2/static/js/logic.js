5b88ecbb0f1a3005c9f598c0c70e87a2008fa11a
yangty152 <35477773+yangty152@users.noreply.github.com>
2021-07-10 00:07:58 -0400

    complete control layers

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,20 +1,6 @@
-// Create an initial map object
-// Set the longitude, latitude, and the starting zoom level
-var myMap = L.map("map", {
-    center: [37.7043,-97.251],
-    zoom: 4
-  });
-
-  var info = L.control({
-    position: "bottomright"
-  });
-
-info.onAdd = function(){
-  var div = L.DomUtil.create("div","legend");
-  return div;
-};
-
-info.addTo(myMap);
+//retrieve data and add Marker and pop up to the map
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then( data => {
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(boundaries=>{
 
 //function to change marker size based on the mag
 function markerSize(mag){
@@ -42,44 +28,99 @@ function fillcolor(depth){
   }; 
 }
 
-// Add a tile layer (the background map image) to our map
+// Add tile layers (the background map image) to our map
 // Use the addTo method to add objects to our map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var Satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: "mapbox/satellite-v9",
   accessToken: API_KEY
-}).addTo(myMap);
+});
 
-//retrieve data and add Marker and pop up to the map
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then( data => {
-  var features = data.features;
+var Dark = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/dark-v10",
+  accessToken: API_KEY
+});
 
-  
-  for (var i=0; i<features.length; i++){
-    var coordinates = features[i].geometry.coordinates;
-    //console.log([coordinates[0],coordinates[1]]);
-    if (coordinates){
-    L.circleMarker([coordinates[1],coordinates[0]], {
+var Outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/outdoors-v10",
+  accessToken: API_KEY
+});
+
+var baseMaps = {
+  Satellite: Satellite,
+  Dark: Dark,
+  Outdoors: Outdoors,
+};
+
+var features = data.features;
+var tectonicPlates = L.geoJson(boundaries);
+
+//Create overlay map layers
+var overlayMaps = {
+  Earthquakes: new L.LayerGroup(),
+  TectonicPlates: tectonicPlates
+};
+
+//draw mark on the map
+for (var i=0; i<features.length; i++){
+  var coordinates = features[i].geometry.coordinates;
+  console.log([coordinates[0],coordinates[1]]);
+  if (coordinates){
+    var earthquakes = L.circleMarker([coordinates[1],coordinates[0]], {
       fillOpacity: 0.75,
       color: "gray",
       weight: 0.5,
       fillColor: fillcolor(coordinates[2]),
       radius: markerSize(features[i].properties.mag),
-      }).bindPopup("<h3>" + features[i].properties.place + "<hr><br/>Mag:"+features[i].properties.mag+"<br/>Coordindates:"+features[i].geometry.coordinates+"</h3>"
-      ).addTo(myMap);
-      }
+    }).bindPopup("<h3>" + features[i].properties.place + "<hr><br/>Mag:"+features[i].properties.mag+"<br/>Coordindates:"+features[i].geometry.coordinates+"</h3>"
+    );
+    earthquakes.addTo(overlayMaps["Earthquakes"]);
     }
-})
+  }
+
+//Create map object
+var myMap = L.map("map", {
+  center: [37.7043,-97.251],
+  zoom: 4,
+  layers: [Satellite, overlayMaps.Earthquakes],
+});
+
+//Add control layers
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
 
 //Add legend to the map
+var info = L.control({
+  position: "bottomright"
+});
+
+info.onAdd = function(){
+var div = L.DomUtil.create("div","legend");
+return div;
+};
+
+info.addTo(myMap);
+
 document.querySelector(".legend").innerHTML = [
-    "<div><div class='box level1'></div>-10-10</div><br>",
-    "<div><div class='box level2'></div>-10-30</div><br>",
-    "<div><div class='box level3'></div>-30-50</div><br>",
-    "<div><div class='box level4'></div>-50-70</div><br>",
-    "<div><div class='box level5'></div>-70-90</div><br>",
-    "<div><div class='box level6'></div>90+</div>",
-  ].join("");
+  "<div><div class='box level1'></div>-10-10</div><br>",
+  "<div><div class='box level2'></div>-10-30</div><br>",
+  "<div><div class='box level3'></div>-30-50</div><br>",
+  "<div><div class='box level4'></div>-50-70</div><br>",
+  "<div><div class='box level5'></div>-70-90</div><br>",
+  "<div><div class='box level6'></div>90+</div>",
+].join("");
+})
+})
+
+
